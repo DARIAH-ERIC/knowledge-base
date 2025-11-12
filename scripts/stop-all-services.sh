c2c5c5b95ca9a83a8436089e9de5d2479df1b94c
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2025-11-12 16:10:12 +0000

    chore: add script to stop all services

---
+++ scripts/stop-all-services.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker compose \
+  --env-file=./apps/knowledge-base/.env.local \
+  -f ./.devcontainer/docker-compose.postgres.yaml \
+  -f ./.devcontainer/docker-compose.typesense.yaml \
+  -f ./.devcontainer/docker-compose.mailpit.yaml \
+  -f ./.devcontainer/docker-compose.drizzle_gateway.yaml \
+  down --volumes
