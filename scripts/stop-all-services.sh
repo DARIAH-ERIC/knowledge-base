af1064b5c2217b3897c43f4a79c771aba22656cb
Stefan Probst <stefanprobst@posteo.de>
2025-11-13 13:24:17 +0100

    chore: format, default values for env vars, container names

--- scripts/stop-all-services.sh
+++ scripts/stop-all-services.sh
@@ -5,5 +5,5 @@ docker compose \
   -f ./.devcontainer/docker-compose.postgres.yaml \
   -f ./.devcontainer/docker-compose.typesense.yaml \
   -f ./.devcontainer/docker-compose.mailpit.yaml \
-  -f ./.devcontainer/docker-compose.drizzle_gateway.yaml \
+  -f ./.devcontainer/docker-compose.drizzle-gateway.yaml \
   down --volumes
