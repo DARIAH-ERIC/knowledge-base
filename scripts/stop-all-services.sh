bf262da8da131dc315d687a5603c8084783d94d1
Stefan Probst <stefanprobst@posteo.de>
2025-11-20 17:06:27 +0100

    docs: update [skip ci]

--- scripts/stop-all-services.sh
+++ scripts/stop-all-services.sh
@@ -2,9 +2,9 @@
 
 docker compose \
   --env-file=./.devcontainer/.env \
-  -f ./.devcontainer/docker-compose.postgres.yaml \
-  -f ./.devcontainer/docker-compose.typesense.yaml \
-  -f ./.devcontainer/docker-compose.mailpit.yaml \
-  -f ./.devcontainer/docker-compose.drizzle-gateway.yaml \
-  -f ./.devcontainer/docker-compose.imgproxy.yaml \
+  --file ./.devcontainer/docker-compose.postgres.yaml \
+  --file ./.devcontainer/docker-compose.typesense.yaml \
+  --file ./.devcontainer/docker-compose.mailpit.yaml \
+  --file ./.devcontainer/docker-compose.drizzle-gateway.yaml \
+  --file ./.devcontainer/docker-compose.imgproxy.yaml \
   down --volumes
