04883119151f89ca69a608444babc7549abe89b5
Stefan Probst <stefanprobst@posteo.de>
2025-11-30 16:36:38 +0100

    feat: add db-client package
    Co-authored-by: Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>

---
+++ packages/database-client/config/drizzle.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "drizzle-kit";
+
+import { env } from "./env.config";
+
+export default defineConfig({
+	casing: "snake_case",
+	dbCredentials: {
+		database: env.DATABASE_NAME,
+		host: env.DATABASE_HOST,
+		password: env.DATABASE_PASSWORD,
+		port: env.DATABASE_PORT,
+		ssl: env.DATABASE_SSL_CONNECTION === "enabled",
+		user: env.DATABASE_USER,
+	},
+	dialect: "postgresql",
+	out: "./db/migrations/",
+	schema: "./src/schema/",
+	strict: true,
+	verbose: true,
+});
