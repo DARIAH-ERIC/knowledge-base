fb0f11d60506af40e40a8e44f8e95130dcb08ea1
stefanprobst <stefanprobst@posteo.de>
2025-12-05 14:59:01 +0100

    feat: add typesense search index for resources (#25)
    this adds a new package for managing the typesense search index.
    
    schema for the "resources" collection:
    https://github.com/DARIAH-ERIC/knowledge-base/blob/01e1593f85fc773d8252ff71b59965770c3eee0d/packages/search-index/src/schema.ts
    
    to create and seed the collection with fake test data:
    
    ```bash
    # in monorepo root
    pnpm search-index:collections:create
    pnpm search-index:collections:seed
    ```
    
    to create a collection and ingest data from openaire, sshoc marketplace,
    and zotero:
    
    ```bash
    # in monorepo root
    pnpm search-index:collections:create
    pnpm search-index:collections:ingest
    ```
    
    note that the ingest script currently times out for sshoc marketplace
    (takes 14s per page - see
    https://github.com/SSHOC/sshoc-marketplace-backend/issues/526)
    
    also fixes the incorrect typesense container port.
    
    ---------
    
    Co-authored-by: Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>

---
+++ packages/search-index/src/client.ts
@@ -0,0 +1,27 @@
+import { assert } from "@acdh-oeaw/lib";
+import { Client } from "typesense";
+
+import { env } from "../config/env.config";
+import { cacheSearchResultsForSeconds } from "../config/search.config";
+
+export function createClient(): Client {
+	const apiKey = env.NEXT_PUBLIC_TYPESENSE_SEARCH_API_KEY;
+	assert(apiKey, "Missing `NEXT_PUBLIC_TYPESENSE_SEARCH_API_KEY` environment variable.");
+
+	const client = new Client({
+		apiKey,
+		cacheSearchResultsForSeconds,
+		connectionTimeoutSeconds: 3,
+		nodes: [
+			{
+				host: env.NEXT_PUBLIC_TYPESENSE_HOST,
+				port: env.NEXT_PUBLIC_TYPESENSE_PORT,
+				protocol: env.NEXT_PUBLIC_TYPESENSE_PROTOCOL,
+			},
+		],
+	});
+
+	return client;
+}
+
+export const client = createClient();
