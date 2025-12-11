26a689df2db7b46db55da9e03d408e1cdc141dfc
Stefan Probst <stefanprobst@posteo.de>
2025-12-11 10:53:20 +0100

    feat: add website skeleton (#29)
    this adds a new application package for the dariah.eu website. currently
    mainly for testing and example pages, but we may want to move all of
    website development from https://github.com/DARIAH-ERIC/dariah-website/
    into the monorepo in the future (still undecided).
    
    also renames redmine_id env var to service_id.
    also defines explicit ports for each package (3001 for knowledge-base,
    3002 for website), and ensures e2e tests run on these ports, so we can
    test all application packages concurrently.
    
    ---------
    
    Co-authored-by: Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>

---
+++ apps/website/next.config.ts
@@ -0,0 +1,86 @@
+import { withSentryConfig } from "@sentry/nextjs";
+import type { NextConfig as Config } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
+
+import { env } from "./config/env.config.ts";
+
+const config: Config = {
+	allowedDevOrigins: ["127.0.0.1"],
+	// cacheComponents: true,
+	/** Compression should be handled by the `nginx` reverse proxy. */
+	compress: false,
+	experimental: {
+		browserDebugInfoInTerminal: true,
+		globalNotFound: true,
+		rootParams: true,
+		turbopackFileSystemCacheForDev: true,
+		viewTransition: true,
+	},
+	headers() {
+		const headers: Awaited<ReturnType<NonNullable<Config["headers"]>>> = [
+			/** @see {@link https://nextjs.org/docs/app/guides/self-hosting#streaming-and-suspense} */
+			{ source: "/:path*{/}?", headers: [{ key: "x-accel-buffering", value: "no" }] },
+		];
+
+		return headers;
+	},
+	logging: {
+		fetches: {
+			fullUrl: true,
+		},
+	},
+	output: env.BUILD_MODE,
+	reactCompiler: true,
+	turbopack: {
+		rules: {
+			/** @see {@link https://github.com/vercel/next.js/discussions/77721#discussioncomment-14576268} */
+			"*": {
+				condition: {
+					all: [
+						"foreign",
+						"browser",
+						{
+							path: /(@react-stately|@react-aria|@react-spectrum|react-aria-components)\/.*\/[a-z]{2}-[A-Z]{2}/,
+						},
+					],
+				},
+				loaders: ["null-loader"],
+				as: "*.js",
+			},
+		},
+	},
+	// typedRoutes: true,
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+};
+
+const plugins: Array<(config: Config) => Config> = [
+	createNextIntlPlugin({
+		experimental: {
+			/** @see {@link https://next-intl.dev/docs/workflows/typescript#messages-arguments} */
+			createMessagesDeclaration: ["./content/en/metadata/index.json", "./messages/en.json"],
+		},
+		requestConfig: "./lib/i18n/request.ts",
+	}),
+	function createSentryPlugin(config) {
+		return withSentryConfig(config, {
+			disableLogger: true,
+			org: env.NEXT_PUBLIC_WEBSITE_SENTRY_ORG,
+			project: env.NEXT_PUBLIC_WEBSITE_SENTRY_PROJECT,
+			reactComponentAnnotation: {
+				enabled: true,
+			},
+			silent: env.CI !== true,
+			/**
+			 * Route browser requests to `sentry` through a `next.js` rewrite to circumvent ad-blockers.
+			 */
+			tunnelRoute: "/monitoring",
+			widenClientFileUpload: true,
+		});
+	},
+];
+
+export default plugins.reduce((config, plugin) => {
+	return plugin(config);
+}, config);
