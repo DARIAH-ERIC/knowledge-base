cb0013a4bb7131376dc5d959502db40933d75759
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2025-12-19 16:48:43 +0000

    feat: add table for allowed relation types

--- packages/database-client/src/schema/organisational-units.ts
+++ packages/database-client/src/schema/organisational-units.ts
@@ -7,7 +7,7 @@ import { assets } from "./assets";
 export const organisationalUnitTypes = ["body", "consortium", "institution"] as const;
 export const organisationalUnitStatus = [
 	"cooperating_partner",
-	"member_country",
+	"member",
 	"national_coordinating_institution",
 	"national_representative_institution",
 	"partner_institution",
@@ -26,7 +26,7 @@ export const organisationalUnits = p.pgTable("organisational_units", {
 });
 
 export const organisationalUnitsRelations = p.pgTable(
-	"units_to_units",
+	"organisational_units_to_units",
 	{
 		unitId: f
 			.uuidv7("unit_id")
@@ -45,13 +45,63 @@ export const organisationalUnitsRelations = p.pgTable(
 		status: p.text("status", { enum: organisationalUnitStatus }).notNull(),
 	},
 	(t) => {
-		return [p.primaryKey({ columns: [t.unitId, t.relatedUnitId], name: "units_to_units_pkey" })];
+		return [
+			p.primaryKey({
+				columns: [t.unitId, t.relatedUnitId],
+				name: "organisational_units_to_units_pkey",
+			}),
+		];
+	},
+);
+
+export const organisationalUnitsAllowedRelations = p.pgTable(
+	"organisational_units_allowed_relations",
+	{
+		unitType: p.text("type", { enum: organisationalUnitTypes }).notNull(),
+		relatedUnitType: p.text("related_unit_type", { enum: organisationalUnitTypes }).notNull(),
+		relationType: p.text("relation_type", { enum: organisationalUnitStatus }).notNull(),
+	},
+	(t) => {
+		return [
+			p.primaryKey({
+				columns: [t.unitType, t.relatedUnitType, t.relationType],
+				name: "organisational_units_allowed_relations_pkey",
+			}),
+		];
 	},
 );
 
 export type OrganisationalUnit = typeof organisationalUnits.$inferSelect;
 export type OrganisationalUnitInput = typeof organisationalUnits.$inferInsert;
 
+export type OrganisationalUnitRelation = typeof organisationalUnitsRelations.$inferSelect;
+export type OrganisationalUnitRelationInput = typeof organisationalUnitsRelations.$inferInsert;
+
+export type OrganisationalUnitAllowedRelation =
+	typeof organisationalUnitsAllowedRelations.$inferSelect;
+export type OrganisationalUnitAllowedRelationInput =
+	typeof organisationalUnitsAllowedRelations.$inferInsert;
+
 export const OrganisationalUnitSelectSchema = createSelectSchema(organisationalUnits);
 export const OrganisationalUnitInsertSchema = createInsertSchema(organisationalUnits);
 export const OrganisationalUnitUpdateSchema = createUpdateSchema(organisationalUnits);
+
+export const OrganisationalUnitRelationSelectSchema = createSelectSchema(
+	organisationalUnitsRelations,
+);
+export const OrganisationalUnitRelationInsertSchema = createInsertSchema(
+	organisationalUnitsRelations,
+);
+export const OrganisationalUnitRelationUpdateSchema = createUpdateSchema(
+	organisationalUnitsRelations,
+);
+
+export const OrganisationalUnitAllowedRelationSelectSchema = createSelectSchema(
+	organisationalUnitsAllowedRelations,
+);
+export const OrganisationalUnitAllowedRelationInsertSchema = createInsertSchema(
+	organisationalUnitsAllowedRelations,
+);
+export const OrganisationalUnitAllowedRelationUpdateSchema = createUpdateSchema(
+	organisationalUnitsAllowedRelations,
+);
