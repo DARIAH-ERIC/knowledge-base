594849b10644c3f454b18b5b3438dd37f398f428
Stefan Probst <stefanprobst@posteo.de>
2025-12-31 20:30:55 +0100

    fix: make .env file optional in services script

--- scripts/services.sh
+++ scripts/services.sh
@@ -23,29 +23,34 @@ for file in "$DEVCONTAINER_DIR"/docker-compose.*.yaml; do
 done
 
 if [ $# -eq 0 ]; then
-	SERVICES="$ALL_SERVICES"
+	SERVICES=$ALL_SERVICES
 else
-	SERVICES="$*"
+	SERVICES=$*
 fi
 
-FILES=""
+set --
+
+if [ -e "$ENV_FILE" ]; then
+	set -- "$@" --env-file "$ENV_FILE"
+fi
+
 for service in $SERVICES; do
 	file="$DEVCONTAINER_DIR/docker-compose.$service.yaml"
 	if [ ! -f "$file" ]; then
 		echo "Unknown service: $service"
 		exit 1
 	fi
-	FILES="$FILES --file $file"
+	set -- "$@" --file "$file"
 done
 
 case "$ACTION" in
 	start)
-		echo "Starting services: $SERVICES"
-		eval docker compose --env-file "\"$ENV_FILE\"" $FILES up --detach
+		echo "Starting services:$SERVICES"
+		docker compose "$@" up --detach
 		;;
 	stop)
-		echo "Stopping services: $SERVICES"
-		eval docker compose --env-file "\"$ENV_FILE\"" $FILES down --volumes
+		echo "Stopping services:$SERVICES"
+		docker compose "$@" down --volumes
 		;;
 	*)
 		echo "Unknown action: $ACTION. Use 'start' or 'stop'."
