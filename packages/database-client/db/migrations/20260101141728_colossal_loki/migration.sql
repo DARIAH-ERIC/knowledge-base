687119fd5f8ab9c0756ce479e8160275352bd6be
Stefan Probst <stefanprobst@posteo.de>
2026-01-01 15:44:12 +0100

    chore: add more licenses

--- packages/database-client/db/migrations/20260101141728_colossal_loki/migration.sql
+++ packages/database-client/db/migrations/20260101141728_colossal_loki/migration.sql
@@ -7,6 +7,7 @@ VALUES
 	('rich_text')
 ON CONFLICT ("type") DO NOTHING;
 
+--> statement-breakpoint
 INSERT INTO
 	"content_blocks_type_data_types" ("type")
 VALUES
@@ -14,6 +15,7 @@ VALUES
 	('news')
 ON CONFLICT ("type") DO NOTHING;
 
+--> statement-breakpoint
 INSERT INTO
 	"entity_status" ("type")
 VALUES
@@ -21,6 +23,7 @@ VALUES
 	('published')
 ON CONFLICT ("type") DO NOTHING;
 
+--> statement-breakpoint
 INSERT INTO
 	"entity_types" ("type")
 VALUES
@@ -32,23 +35,43 @@ VALUES
 	('spotlight_articles')
 ON CONFLICT ("type") DO NOTHING;
 
+--> statement-breakpoint
 INSERT INTO
-	"licenses" ("name", "url")
+	"licenses" ("code", "name", "url")
 VALUES
 	(
-		'CC0 1.0',
+		'CC0-1.0',
+		'Creative Commons Zero v1.0 Universal',
 		'https://creativecommons.org/publicdomain/zero/1.0/'
 	),
 	(
-		'CC BY 4.0',
+		'CC-BY-4.0',
+		'Creative Commons Attribution 4.0 International',
 		'https://creativecommons.org/licenses/by/4.0/'
 	),
 	(
-		'CC BY-SA 4.0',
-		'https://creativecommons.org/licenses/by-sa/4.0/'
+		'CC-BY-NC-4.0',
+		'Creative Commons Attribution Non Commercial 4.0 International',
+		'https://creativecommons.org/licenses/by-nc/4.0/'
 	),
 	(
-		'CC BY-NC-SA 4.0',
+		'CC-BY-NC-ND-4.0',
+		'Creative Commons Attribution Non Commercial No Derivatives 4.0 International',
+		'https://creativecommons.org/licenses/by-nc-nd/4.0/'
+	),
+	(
+		'CC-BY-NC-SA-4.0',
+		'Creative Commons Attribution Non Commercial Share Alike 4.0 International',
 		'https://creativecommons.org/licenses/by-nc-sa/4.0/'
+	),
+	(
+		'CC-BY-ND-4.0',
+		'Creative Commons Attribution No Derivatives 4.0 International',
+		'https://creativecommons.org/licenses/by-nd/4.0/'
+	),
+	(
+		'CC-BY-SA-4.0',
+		'Creative Commons Attribution Share Alike 4.0 International',
+		'https://creativecommons.org/licenses/by-sa/4.0/'
 	)
-ON CONFLICT ("name") DO NOTHING;
+ON CONFLICT ("code") DO NOTHING;
