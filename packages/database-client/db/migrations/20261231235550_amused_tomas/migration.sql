16fb69f4069a326fc5d504c25202fda0345462b6
Stefan Probst <stefanprobst@posteo.de>
2026-01-01 13:59:47 +0100

    fix: use single quotes for string literals

--- packages/database-client/db/migrations/20261231235550_amused_tomas/migration.sql
+++ packages/database-client/db/migrations/20261231235550_amused_tomas/migration.sql
@@ -1,33 +1,33 @@
 INSERT INTO
 	"content_block_types" ("type")
 VALUES
-	("data"),
-	("embed"),
-	("image"),
-	("rich_text")
+	('data'),
+	('embed'),
+	('image'),
+	('rich_text')
 ON CONFLICT DO NOTHING;
 
 INSERT INTO
 	"content_blocks_type_data_types" ("type")
 VALUES
-	("events"),
-	("news")
+	('events'),
+	('news')
 ON CONFLICT DO NOTHING;
 
 INSERT INTO
 	"entity_status" ("type")
 VALUES
-	("draft"),
-	("published")
+	('draft'),
+	('published')
 ON CONFLICT DO NOTHING;
 
 INSERT INTO
 	"entity_types" ("type")
 VALUES
-	("events"),
-	("impact_case_studies"),
-	("news"),
-	("pages"),
-	("persons"),
-	("spotlight_articles")
+	('events'),
+	('impact_case_studies'),
+	('news'),
+	('pages'),
+	('persons'),
+	('spotlight_articles')
 ON CONFLICT DO NOTHING;
