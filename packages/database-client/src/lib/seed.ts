954fd30b620f550d589d911c6c430ca4d36e18ea
Stefan Probst <stefanprobst@posteo.de>
2026-01-04 21:10:04 +0100

    chore: format

--- packages/database-client/src/lib/seed.ts
+++ packages/database-client/src/lib/seed.ts
@@ -99,7 +99,7 @@ export async function seed(db: Client, config: SeedConfig = {}): Promise<void> {
 			return type;
 		});
 
-		const persons: Array<Omit<schema.PersonInput, 'id'>> = f.helpers.multiple(
+		const persons: Array<Omit<schema.PersonInput, "id">> = f.helpers.multiple(
 			() => {
 				const firstName = f.person.firstName();
 				const lastName = f.person.lastName();
@@ -134,7 +134,7 @@ export async function seed(db: Client, config: SeedConfig = {}): Promise<void> {
 			}),
 		);
 
-		const events: Array<Omit<schema.EventInput, 'id'>> = f.helpers.multiple(
+		const events: Array<Omit<schema.EventInput, "id">> = f.helpers.multiple(
 			() => {
 				const title = f.lorem.sentence();
 				const startDate = f.date.past({ years: 5 });
@@ -206,7 +206,7 @@ export async function seed(db: Client, config: SeedConfig = {}): Promise<void> {
 			}),
 		);
 
-		const impactCaseStudies: Array<Omit<schema.ImpactCaseStudyInput, 'id'>> = f.helpers.multiple(
+		const impactCaseStudies: Array<Omit<schema.ImpactCaseStudyInput, "id">> = f.helpers.multiple(
 			() => {
 				const title = f.lorem.sentence();
 
@@ -251,7 +251,7 @@ export async function seed(db: Client, config: SeedConfig = {}): Promise<void> {
 
 		await db.insert(schema.impactCaseStudiesToPersons).values(impactCaseStudiesToPersons);
 
-		const news: Array<Omit<schema.NewsItemInput, 'id'>> = f.helpers.multiple(
+		const news: Array<Omit<schema.NewsItemInput, "id">> = f.helpers.multiple(
 			() => {
 				const title = f.lorem.sentence();
 
@@ -284,7 +284,7 @@ export async function seed(db: Client, config: SeedConfig = {}): Promise<void> {
 			}),
 		);
 
-		const pages: Array<Omit<schema.PageInput, 'id'>> = f.helpers.multiple(
+		const pages: Array<Omit<schema.PageInput, "id">> = f.helpers.multiple(
 			() => {
 				const title = f.lorem.sentence();
 
@@ -317,7 +317,7 @@ export async function seed(db: Client, config: SeedConfig = {}): Promise<void> {
 			}),
 		);
 
-		const spotlightArticles: Array<Omit<schema.SpotlightArticleInput, 'id'>> = f.helpers.multiple(
+		const spotlightArticles: Array<Omit<schema.SpotlightArticleInput, "id">> = f.helpers.multiple(
 			() => {
 				const title = f.lorem.sentence();
 
