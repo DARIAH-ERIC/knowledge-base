502f20b91e3102c45e64dee1c2b305f0a352c12d
Stefan Probst <stefanprobst@posteo.de>
2026-01-19 14:34:03 +0100

    chore: fix types

--- apps/knowledge-base/app/(app)/[locale]/(dashboard)/dashboard/website/assets/_lib/upload-image.schema.ts
+++ apps/knowledge-base/app/(app)/[locale]/(dashboard)/dashboard/website/assets/_lib/upload-image.schema.ts
@@ -1,6 +1,7 @@
 import * as v from "valibot";
 
 import { imageMimeTypes, imageSizeLimit } from "@/config/assets.config";
+import { assetPrefixes } from "@/lib/data/assets";
 
 export const UploadImageInputSchema = v.object({
 	file: v.pipe(
@@ -10,4 +11,6 @@ export const UploadImageInputSchema = v.object({
 			return input.size <= imageSizeLimit;
 		}),
 	),
+	licenseId: v.optional(v.pipe(v.string(), v.nonEmpty())),
+	prefix: v.picklist(assetPrefixes),
 });
