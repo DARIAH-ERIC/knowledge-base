3ee94c531d4afcd46104f737d103559b4ff61b56
Stefan Probst <stefanprobst@posteo.de>
2026-01-30 18:34:30 +0100

    chore: fix typo in openapi tags (#114)
    this fixes a typo in openapi tags ("members-partners" vs
    "members-and-partners") to ensure endpoints are correctly grouped
    according to their tags.

--- apps/api-server/src/routes/members-partners/index.ts
+++ apps/api-server/src/routes/members-partners/index.ts
@@ -18,7 +18,7 @@ import {
 
 export const router = createRouter()
 	/**
-	 * GET /api/members-and-partners
+	 * GET /api/members-partners
 	 */
 	.get(
 		"/",
@@ -55,12 +55,12 @@ export const router = createRouter()
 	)
 
 	/**
-	 * GET /api/members-and-partners/:id
+	 * GET /api/members-partners/:id
 	 */
 	.get(
 		"/:id",
 		describeRoute({
-			tags: ["members-and-partners"],
+			tags: ["members-partners"],
 			summary: "Get member or partner by id",
 			description: "Retrieve a member or partner by id",
 			operationId: "getMembersAndPartnersById",
@@ -97,12 +97,12 @@ export const router = createRouter()
 	)
 
 	/**
-	 * GET /api/members-and-partners/slugs/:slug
+	 * GET /api/members-partners/slugs/:slug
 	 */
 	.get(
 		"/slugs/:slug",
 		describeRoute({
-			tags: ["members-and-partners"],
+			tags: ["members-partners"],
 			summary: "Get member or partner by slug",
 			description: "Retrieve a member or partner by slug",
 			operationId: "getMemberOrPartnerBySlug",
