0f0e66a708c9c20bd6b59b44b5b853e5b17b65e5
Stefan Probst <stefanprobst@posteo.de>
2026-03-10 17:19:24 +0100

    feat: add working groups endpoints

---
+++ apps/api/test/api-working-groups.test.ts
@@ -0,0 +1,251 @@
+import { assert } from "@acdh-oeaw/lib";
+import * as schema from "@dariah-eric/database/schema";
+import { faker as f } from "@faker-js/faker";
+import slugify from "@sindresorhus/slugify";
+import { v7 as uuidv7 } from "uuid";
+import { describe, expect, it } from "vitest";
+
+import type { Database } from "@/middlewares/db";
+import { createTestClient } from "~/test/lib/create-test-client";
+import { withTransaction } from "~/test/lib/with-transaction";
+
+function createItems(count: number) {
+	const items = f.helpers.multiple(
+		() => {
+			const id = uuidv7();
+			const documentId = uuidv7();
+			const name = f.lorem.sentence();
+			const slug = slugify(name);
+
+			const entity = {
+				id,
+				slug,
+				documentId,
+			};
+
+			const organisationalUnit = {
+				id,
+				name,
+				summary: f.lorem.paragraph(),
+			};
+
+			return { entity, organisationalUnit };
+		},
+		{ count },
+	);
+
+	return items;
+}
+
+async function seed(db: Database, items: ReturnType<typeof createItems>) {
+	const [status, entityType, asset, workingGroupType] = await Promise.all([
+		db.query.entityStatus.findFirst({ columns: { id: true }, where: { type: "published" } }),
+		db.query.entityTypes.findFirst({
+			columns: { id: true },
+			where: { type: "organisational_units" },
+		}),
+		db.query.assets.findFirst({ columns: { id: true } }),
+		db.query.organisationalUnitTypes.findFirst({
+			columns: { id: true },
+			where: { type: "working_group" },
+		}),
+	]);
+
+	assert(status, "No entity status in database.");
+	assert(entityType, "No entity type in database.");
+	assert(asset, "No assets in database.");
+	assert(workingGroupType, "No working_group type in database.");
+
+	await db.insert(schema.entities).values(
+		items.map((item) => {
+			return { ...item.entity, statusId: status.id, typeId: entityType.id };
+		}),
+	);
+
+	await db.insert(schema.organisationalUnits).values(
+		items.map((item) => {
+			return { ...item.organisationalUnit, typeId: workingGroupType.id, imageId: asset.id };
+		}),
+	);
+}
+
+describe("working-groups", () => {
+	describe("GET /api/working-groups", () => {
+		it("should return paginated list of working groups", async () => {
+			await withTransaction(async (db) => {
+				const limit = 10;
+				const offset = 0;
+
+				const client = createTestClient(db);
+
+				const items = createItems(3);
+				await seed(db, items);
+
+				const item = items.at(1)!;
+				const name = item.organisationalUnit.name;
+
+				const response = await client["working-groups"].$get({
+					query: {
+						limit: String(limit),
+						offset: String(offset),
+					},
+				});
+
+				expect(response.status).toBe(200);
+
+				const data = await response.json();
+
+				expect(data.total).toBeGreaterThanOrEqual(items.length);
+				expect(data.data).toEqual(expect.arrayContaining([expect.objectContaining({ name })]));
+				expect(data.limit).toBe(limit);
+				expect(data.offset).toBe(offset);
+			});
+		});
+	});
+
+	describe("GET /api/working-groups/:id", () => {
+		it("should return single working group", async () => {
+			await withTransaction(async (db) => {
+				const client = createTestClient(db);
+
+				const items = createItems(3);
+				await seed(db, items);
+
+				const item = items.at(1)!;
+				const id = item.entity.id;
+				const name = item.organisationalUnit.name;
+
+				const response = await client["working-groups"][":id"].$get({
+					param: {
+						id,
+					},
+				});
+
+				expect(response.status).toBe(200);
+
+				const data = await response.json();
+
+				expect(data).toMatchObject({ name });
+			});
+		});
+
+		it("should return 400 for invalid id", async () => {
+			await withTransaction(async (db) => {
+				const client = createTestClient(db);
+
+				const items = createItems(3);
+				await seed(db, items);
+
+				const id = "no-uuid";
+
+				const response = await client["working-groups"][":id"].$get({
+					param: {
+						id,
+					},
+				});
+
+				expect(response.status).toBe(400);
+			});
+		});
+
+		it("should return 404 for non-existing id", async () => {
+			await withTransaction(async (db) => {
+				const client = createTestClient(db);
+
+				const items = createItems(3);
+				await seed(db, items);
+
+				const id = "019b75fd-6d6a-757c-acc2-c3c6266a0f31";
+
+				const response = await client["working-groups"][":id"].$get({
+					param: {
+						id,
+					},
+				});
+
+				expect(response.status).toBe(404);
+			});
+		});
+	});
+
+	describe("GET /api/working-groups/slugs", () => {
+		it("should return paginated list of slugs", async () => {
+			await withTransaction(async (db) => {
+				const limit = 10;
+				const offset = 0;
+
+				const client = createTestClient(db);
+
+				const items = createItems(3);
+				await seed(db, items);
+
+				const item = items.at(1)!;
+				const slug = item.entity.slug;
+
+				const response = await client["working-groups"].slugs.$get({
+					query: {
+						limit: String(limit),
+						offset: String(offset),
+					},
+				});
+
+				expect(response.status).toBe(200);
+
+				const data = await response.json();
+
+				expect(data.total).toBeGreaterThanOrEqual(items.length);
+				expect(data.data).toEqual(
+					expect.arrayContaining([expect.objectContaining({ entity: { slug } })]),
+				);
+				expect(data.limit).toBe(limit);
+				expect(data.offset).toBe(offset);
+			});
+		});
+	});
+
+	describe("GET /api/working-groups/slugs/:slug", () => {
+		it("should return single working group", async () => {
+			await withTransaction(async (db) => {
+				const client = createTestClient(db);
+
+				const items = createItems(3);
+				await seed(db, items);
+
+				const item = items.at(1)!;
+				const slug = item.entity.slug;
+				const name = item.organisationalUnit.name;
+
+				const response = await client["working-groups"].slugs[":slug"].$get({
+					param: {
+						slug,
+					},
+				});
+
+				expect(response.status).toBe(200);
+
+				const data = await response.json();
+
+				expect(data).toMatchObject({ name });
+			});
+		});
+
+		it("should return 404 for non-existing slug", async () => {
+			await withTransaction(async (db) => {
+				const client = createTestClient(db);
+
+				const items = createItems(3);
+				await seed(db, items);
+
+				const slug = "non-existing-slug";
+
+				const response = await client["working-groups"].slugs[":slug"].$get({
+					param: {
+						slug,
+					},
+				});
+
+				expect(response.status).toBe(404);
+			});
+		});
+	});
+});
