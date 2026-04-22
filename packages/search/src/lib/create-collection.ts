dd62944e78dede5578132a8eabbe6307582b51e2
Stefan Probst <stefanprobst@posteo.de>
2026-04-22 10:37:54 +0200

    chore: lint

--- packages/search/src/lib/create-collection.ts
+++ packages/search/src/lib/create-collection.ts
@@ -1,9 +1,10 @@
 import type { Collection } from "../create-collection";
 import type { Client } from "./admin-client";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+
 export async function createCollection(
 	client: Client,
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	collection: Collection<any, any, any, any>,
 ): Promise<boolean> {
 	if (await client.collections(collection.name).exists()) {
