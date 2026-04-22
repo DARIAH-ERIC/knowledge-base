7cf934deed1f36d2eb1ec66614c7c2e440471782
Stefan Probst <stefanprobst@posteo.de>
2026-04-22 10:44:12 +0200

    chore: format

--- packages/search/src/lib/create-collection.ts
+++ packages/search/src/lib/create-collection.ts
@@ -1,7 +1,6 @@
 import type { Collection } from "../create-collection";
 import type { Client } from "./admin-client";
 
-
 export async function createCollection(
 	client: Client,
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
