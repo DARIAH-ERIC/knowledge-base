309d03af3ec851273e1b1a8d5dbe017b9c3be6e5
Stefan Probst <stefanprobst@posteo.de>
2026-05-12 08:32:38 +0200

    chore: update typescript to v6

--- packages/configs/types/utilities.d.ts
+++
@@ -1,3 +0,0 @@
-type DistributiveOmit<T, K extends PropertyKey> = T extends unknown ? Omit<T, K> : never;
-
-type DistributivePick<T, K extends PropertyKey> = T extends unknown ? Pick<T, K> : never;
