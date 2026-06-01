91126b0eec4d078b1db5f8447597542616cf01b8
Stefan Probst <stefanprobst@posteo.de>
2026-06-01 16:35:56 +0200

    fix: don't wipe subtype on project publish

--- apps/knowledge-base/lib/data/projects.lifecycle-adapter.ts
+++ apps/knowledge-base/lib/data/projects.lifecycle-adapter.ts
@@ -49,4 +49,45 @@ export const projectsLifecycleAdapter: EntityLifecycleAdapter = {
 			.where(eq(schema.projectsToSocialMedia.projectId, versionId));
 		await tx.delete(schema.projects).where(eq(schema.projects.id, versionId));
 	},
+
+	async replaceSubtype(tx, sourceVersionId, targetVersionId) {
+		const [source] = await tx
+			.select({
+				acronym: schema.projects.acronym,
+				call: schema.projects.call,
+				duration: schema.projects.duration,
+				funding: schema.projects.funding,
+				imageId: schema.projects.imageId,
+				metadata: schema.projects.metadata,
+				name: schema.projects.name,
+				scopeId: schema.projects.scopeId,
+				summary: schema.projects.summary,
+				topic: schema.projects.topic,
+			})
+			.from(schema.projects)
+			.where(eq(schema.projects.id, sourceVersionId))
+			.limit(1);
+		if (source == null) {
+			return;
+		}
+
+		await tx
+			.delete(schema.projectsToSocialMedia)
+			.where(eq(schema.projectsToSocialMedia.projectId, targetVersionId));
+
+		await tx.update(schema.projects).set(source).where(eq(schema.projects.id, targetVersionId));
+
+		const socialMedia = await tx
+			.select({ socialMediaId: schema.projectsToSocialMedia.socialMediaId })
+			.from(schema.projectsToSocialMedia)
+			.where(eq(schema.projectsToSocialMedia.projectId, sourceVersionId));
+
+		if (socialMedia.length > 0) {
+			await tx.insert(schema.projectsToSocialMedia).values(
+				socialMedia.map((s) => {
+					return { projectId: targetVersionId, ...s };
+				}),
+			);
+		}
+	},
 };
